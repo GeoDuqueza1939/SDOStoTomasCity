fdbe9d3bc33c39b9eca6f46b8d75f881bee0f397
GeoDuqueza1939 <geovaniduqueza1939@yahoo.com>
2023-03-22 07:39:57 +0800

    Changed location of some code files

---
+++ web/php/classes/dbObject.php
@@ -0,0 +1,47 @@
+<?php
+$baseDir = '/var/www/html';
+require_once("$baseDir/php/classes/db.php");
+
+enum dbObjectStatus
+{
+    case Unlinked; // newly created dbObject; SHOULD NOT ALLOW ANY EDITS TO PRIVATE DATA!!!
+    case Linked; // successfully added or retrieved dbObject
+    case Edited; // object that has been edited and needs to be saved/updated in database
+}
+
+interface dbObjectInterface
+{
+    public function add() : mixed; // Add to database; return PRIMARY KEY
+    public function addFromJson($Json) : mixed; // Add to database from JSON; Useful in extracting data send from clients; return PRIMARY KEY
+    public function retrieve($primaryKey) : bool; // Retrieve from database
+    public function save() : bool; // Update changes to database
+    public function delete() : bool; // Remove from database; AVOID USING THIS!!!
+    public function to_array() : array; // Useful in jsonSerialize
+}
+
+abstract class dbObject implements JsonSerializable, dbObjectInterface
+{
+    public ?DatabaseConnection $dbconn = null; // dbObjects shall share one database connection
+    private dbObjectStatus $dbObjstatus = dbObjectStatus::Unlinked;
+    protected $criteriaStr = ''; // update once the dbObject is linked; USE ONLY FOR DIRECT CHILDREN OF dbObject
+    protected $updateStr = ''; // update everytime a field is modified or the dbObject is saved; USE ONLY FOR DIRECT CHILDREN OF dbObject
+
+    protected function initialize()
+    {}
+
+    public function getDBObjectStatus() : dbObjectStatus
+    {
+        return $this->dbObjstatus;
+    }
+
+    protected function setDBObjectStatus(dbObjectStatus $status)
+    {
+        $this->dbObjstatus = $status;
+    }
+
+    public function jsonSerialize(): mixed
+    {
+        return $this->to_array();
+    }
+}
+?>
