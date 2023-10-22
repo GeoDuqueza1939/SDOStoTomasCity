69ddfea95b3dcb523b3888dbd3251e64cf9bed4b
Geovani Duqueza <geovaniduqueza1939@yahoo.com>
2023-10-22 21:21:15 +0800

    Added spinning logo transition to login page

--- web/local/mpasis/php/classes/mpasisApp.php
+++ web/local/mpasis/php/classes/mpasisApp.php
@@ -57,7 +57,53 @@ if (isValidUserSession())
 }
 else
 {
-    echo('NO USER<br>');
+                // echo('NO USER<br>');
+                ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>SDO Sto. Tomas City | Online Services</title>
+<style>
+@keyframes logospin {
+    0% {transform: rotateY(0deg)};
+    12.5% {transform: rotateY(45deg)};
+    25% {transform: rotateY(90deg)};
+    37.5% {transform: rotateY(135deg)};
+    50% {transform: rotateY(180deg)};
+    62.5% {transform: rotateY(225deg)};
+    75% {transform: rotateY(270deg)};
+    87.5% {transform: rotateY(315deg)};
+    100% {transform: rotateY(0deg)};
+}
+
+.no-user-scrim {
+    position: fixed;
+    top: 0.5em;
+    bottom: 0.5em;
+    left: 0.5em;
+    right: 0.5em;
+    background-color: navy;
+    display: flex;
+    flex-wrap: wrap;
+    align-content: center;
+    justify-content: center;
+}
+
+.sdo-logo {
+    background-image: url('/images/logo-depedstotomas.webp');
+    background-size: contain;
+    width: 80%;
+    height: 80%;
+    background-repeat: no-repeat;
+    background-position: center;
+    animation: logospin 4s infinite linear;
+    transform: rotateY(360deg);
+}
+</style>
+</head>
+<body><div class="no-user-scrim"><div class="sdo-logo"></div></div></body>
+</html>
+                <?php
 
     if ($requiresSignIn)
     {
